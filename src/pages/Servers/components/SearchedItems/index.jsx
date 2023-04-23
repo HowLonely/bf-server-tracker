528089b16433ff317b22cbb7fb21d73c2597cd7f
HowLonely <ellian.andre1203@gmail.com>
2023-04-23 15:38:54 -0400

    Correción lógica buscador

--- src/pages/Servers/components/SearchedItems/index.jsx
+++ src/pages/Servers/components/SearchedItems/index.jsx
@@ -4,10 +4,10 @@ import { ServerSearchContext } from "../../../../context/ServerSearch/ServerSear
 export const SearchedItems = () => {
   const { data, loading, selected, name } = useContext(ServerSearchContext);
 
-  if (loading) return <div>Loading ...</div>;
   if (!selected) return <div></div>;
   if (!name) return <div></div>;
-  if (data.servers.length == 0 && !loading) return <div>No matches</div>;
+  if (loading) return <div>Loading ...</div>;
+  if (data?.servers.length == 0 && !loading) return <div>No matches</div>;
 
-  return <div>{data.servers.map((server) => server.prefix)}</div>;
+  return <div>{data?.servers.map((server) => <div key={server.serverId}>{server.prefix}</div>)}</div>;
 };
